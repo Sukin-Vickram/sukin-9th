a79e96067a0e5a21644e768cc51978e5a29890e8
Sukin-Vickram <avsukin@gmail.com>
2022-01-02 07:13:35 +0530

    testing

---
+++ venv/Basic Calculator.py
@@ -0,0 +1,24 @@
+# Addition
+print("Addition")
+num1 = input("Enter a number :")
+num2 = input("Enter the second number :")
+sum = float(num1) + float(num2)
+print(sum)
+# Subtraction
+print("Subtraction")
+num1 = input("Enter a number :")
+num2 = input("Enter the second number :")
+difference = float(num1) - float(num2)
+print(difference)
+# Multiplication
+print("Multiplication")
+num1 = input("Enter a number :")
+num2 = input("Enter the second number :")
+product = float(num1) * float(num2)
+print(product)
+# Division
+print("Division")
+num1 = input("Enter a number :")
+num2 = input("Enter the second number :")
+quotient = float(num1) / float(num2)
+print(quotient)
